77634b01d21c27736b94a0fac9ec545c9ee38fe8
Mary Cabutotan <maryca452007@gmail.com>
2026-03-31 18:32:03 -0400

    Update course.cpp

--- course.cpp
+++ course.cpp
@@ -9,4 +9,22 @@ Course::Course(const string&);
 {
 }
 
-string Course::getcourse_number()
+string Course::getcourse_number() const
+{
+  return course_number;
+}
+
+string Course::getcourse_name() const
+{
+  return course_number;
+}
+
+string Course::getcourse_() const
+{
+  return course_number;
+}
+
+string Course::getcourse_number() const
+{
+  return course_number;
+}
