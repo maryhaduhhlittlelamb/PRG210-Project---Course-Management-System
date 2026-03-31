3e62f7a593d9d4e0ada713ba5ba33d9d63061a57
Mary Cabutotan <maryca452007@gmail.com>
2026-03-31 19:33:44 -0400

    Update course.h

--- course.h
+++ course.h
@@ -9,17 +9,17 @@ class Course
 private:
     string course_number;
     string course_name;
-    string schedule;
-    double price;
+    string course_schedule;
+    double course_price;
 
 public
     Course();
-    Course(const string& course_number, const string& course_name, const string& schedule, const double price);
+    Course(const string& course_number, const string& course_name, const string& couse_schedule, const double course_price);
 
     string getcourse_number() const;
     string getcourse_name() const;
-    string getschedule() const;
-    double getprice() const;
+    string getcourse_schedule() const;
+    double getcourse_price() const;
 
     void printCourse() const;
 };
