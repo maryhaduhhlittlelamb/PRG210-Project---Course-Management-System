f808b5a421da103ae6c65b67f78dc163da2a1f34
Mary Cabutotan <maryca452007@gmail.com>
2026-03-31 18:10:36 -0400

    Update course.h

--- course.h
+++ course.h
@@ -1,1 +1,25 @@
+#ifndef COURSE_H
+#define COURSE_H
 
+#include <string>
+using namespace std;
+
+class Course {
+private:
+    string course_number;
+    string course_name;
+    string schedule;
+    double price;
+public
+    Course();
+    Course(const string& course_number, const string& course_name, const string& schedule, const double price);
+
+    string getcourse_number() const;
+    string getcourse_name() const;
+    string getschedule() const;
+    double getprice() const;
+
+    void printCourse() const;
+};
+
+#endif
