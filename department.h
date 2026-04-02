f334fb3a76bd9e92dbac6c828c9de2722116f1f3
Mary Cabutotan <maryca452007@gmail.com>
2026-04-02 17:09:48 -0400

    Update department.h

--- department.h
+++ department.h
@@ -7,9 +7,19 @@ using namespace std;
 class Department
 {
 private:
-   
+    string department_name;
+    Course* course_list;
+    int course_count;
 
 public:
+    Department();
+    Department(const string& name);
+    ~Department();
+
+    void addCourse(const Course& c);
+    void printCourses() const;
+
+    string getdepartment_name() const;
 };
 
 #endif
