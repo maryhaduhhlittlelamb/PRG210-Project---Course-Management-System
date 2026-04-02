ef65ed0503d2064b872b0fe621edc1e1dadd6047
Mary Cabutotan <maryca452007@gmail.com>
2026-04-02 18:04:37 -0400

    Update department.cpp

--- department.cpp
+++ department.cpp
@@ -1,1 +1,28 @@
+#include "department.h"
+#include <iostream>
+using namespace std;
 
+Department::Department()
+{
+  department_name = "";
+  course_list = nullptr;
+  course_count = 0;
+}
+
+Department::~Department()
+{
+  delete[] course_list;
+}
+
+void
+{
+}
+
+void
+{
+}
+
+string Department::getdepartment_name() const
+{
+  return department_name;
+}
