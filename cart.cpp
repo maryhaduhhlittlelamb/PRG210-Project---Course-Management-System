1c5b30a01636e8d24a0bdeaf65634cbe92bdd900
Mary Cabutotan <maryca452007@gmail.com>
2026-04-02 19:43:28 -0400

    Update cart.cpp

--- cart.cpp
+++ cart.cpp
@@ -15,17 +15,65 @@ Cart::~Cart()
 
 void Cart::addCourse(const Course& c)
 {
-  Course temp = new Course[cart_count + 1];
+  Course* temp = new Course[cart_count + 1];
+
+  for (int i = 0; i < cart_count; i++)
+    temp[i] = cart_list[i];
+
+  temp[cart_count] = c;
+
+  delete[] cart_list;
+  cart_list = temp;
+  cart_count++;
 }
 
-void
+void Cart::RemoveCourse(const string& courseNumber)
 {
+  if (cart_count == 0)
+    return;
+
+  int index = -1;
+
+  for (int i = 0; i < cart_count; i++)
+    {
+      if (cart_list[i].getcourse_number()
+      {
+        index = i;
+        break;
+      }
+    }
+
+  if(index == -1)
+    return;
+
+  Course* temp = new Course[cart_count - 1];
+
+  for (int i = 0, j = 0; i < cart_count; i++)
+    {
+      if (i != index)
+      {
+        temp[j] = cart_list[i];
+        j++;
+      }
+    }
+
+  delete[] cart_list;
+  cart_list = temp;
+  cart_count--;
 }
 
-void
+void Cart::printCart() const
 {
+  for (int i = 0; i < cart_count; i++)
+    cart_list[i].printCourse();
 }
 
-double 
+double Cart::getTotal() const
 {
+  double total = 0.0;
+
+  for (int i = 0; i < cart_count; i++)
+    total += cart_list[i].getcourse_cost();
+
+  return total;
 }
