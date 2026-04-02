63462a90991f53e6596c6c4426c29e3103bad3a4
Mary Cabutotan <maryca452007@gmail.com>
2026-04-02 19:08:20 -0400

    Update cart.cpp

--- cart.cpp
+++ cart.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 Cart::Cart()
 {
+  cart_list = nullptr;
+  cart_count = 0;
 }
 
 Cart::~Cart()
 {
+  delete[] cart_list;
 }
 
-void
+void Cart::addCourse(const Course& c)
 {
+  Course temp = new Course[cart_count + 1];
 }
 
 void
