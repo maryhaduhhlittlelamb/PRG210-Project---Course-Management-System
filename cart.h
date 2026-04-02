bff0ac2b130a8714b9d583eac41f96f4c241b76f
Mary Cabutotan <maryca452007@gmail.com>
2026-04-02 19:02:07 -0400

    Update cart.h

--- cart.h
+++ cart.h
@@ -7,8 +7,17 @@ using namespace std;
 class Cart 
 {
 private:
+    Course* cart_list;
+    int cart_count;
 
 public:
+    Cart();
+    ~Cart();
+
+    void addCourse(const Course& c);
+    void removeCourse(const string& courseNumber);
+    void printCart() const;
+    double getTotal() const;
 };
 
 #endif
