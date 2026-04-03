3a600d2aa5b807bf707b0dcacd80f812f1bb4dfe
Mary Cabutotan <maryca452007@gmail.com>
2026-04-03 13:41:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+#include "course.h"
+#include "department.h"
+#icnlude "cart.h"
+using namespace std;
 
+void admin_menu();
+void student_menu();
+
+int main()
+{
+
+  return 0;
+}
+
+void admin_menu()
+{
+  
+}
+
+void student_menu()
+{
+  
+}
