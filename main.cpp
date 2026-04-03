38ba1403c0dfe7a88f619520a59ff53827bd1bfa
Mary Cabutotan <maryca452007@gmail.com>
2026-04-03 14:22:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,21 +10,21 @@ void student_menu(Department deptList[], int deptCount, Cart& cart);
 
 int main()
 {
-  const int deptCount = 3 
+  const int deptCount = 3;
   Department deptList[deptCount] = {
       Department("School of Information Technology Administration & Security (ITAS)"),
       Department("School of Software Design & Data Science (SDDS)"),
       Department("School of Electronics & Computer Engineering Technology (ECET)")
   };
 
-  deptlist[0].addCourse(Course("ITAS101", "Networking Fundamentals", "MWF 8-10", 500));
-  deptlist[0].addCourse(Course("ITAS201", "IT Security Basics", "TTh 2-4", 600));
+  deptList[0].addCourse(Course("ITAS101", "Networking Fundamentals", "MWF 8-10", 500));
+  deptList[0].addCourse(Course("ITAS201", "IT Security Basics", "TTh 2-4", 600));
 
-  deptlist[1].addCourse(Course("SDDS101", "Intro to Programming", "MWF 11-1", 520));
-  deptlist[1].addCourse(Course("SDDS201", "Object Oriented Programming", "TTh 11-1", 620));
+  deptList[1].addCourse(Course("SDDS101", "Intro to Programming", "MWF 11-1", 520));
+  deptList[1].addCourse(Course("SDDS201", "Object Oriented Programming", "TTh 11-1", 620));
 
-  deptlist[2].addCourse(Course("ECET101", "Digital Systems", "MWF 2-4", 550));
-  deptlist[2].addCourse(Course("ECET201", "Software Engineering", "TTh 8-10", 650));
+  deptList[2].addCourse(Course("ECET101", "Digital Systems", "MWF 2-4", 550));
+  deptList[2].addCourse(Course("ECET201", "Software Engineering", "TTh 8-10", 650));
 
   Cart cart;
   int choice = 0;
