f527e8199142b257927d09838646cb676b367522
Mary Cabutotan <maryca452007@gmail.com>
2026-04-03 13:56:16 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,25 @@
 #icnlude "cart.h"
 using namespace std;
 
-void admin_menu();
-void student_menu();
+void admin_menu(Department deptList[], int deptCount);
+void student_menu(Department deptList[], int deptCount, Cart& cart);
 
 int main()
 {
+  const int deptCount = 3 
+  Department deptList[deptCount] = {
+      Department("School of Information Technology Administration & Security (ITAS)"),
+      Department("School of Software Design & Data Science (SDDS)"),
+      Department("School of Electronics & Computer Engineering Technology (ECET)")
+  };
+  deptlist[].addCourse(Course("number", "name", "schedule", cost));
+  deptlist[].addCourse(Course("number", "name", "schedule", cost));
+
+  deptlist[].addCourse(Course("number", "name", "schedule", cost));
+  deptlist[].addCourse(Course("number", "name", "schedule", cost));
+
+  deptlist[].addCourse(Course("number", "name", "schedule", cost));
+  deptlist[].addCourse(Course("number", "name", "schedule", cost));
 
   return 0;
 }
