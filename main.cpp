4fc92c00fe99c95ce5ae690dc4ac4da74a08fba0
Mary Cabutotan <maryca452007@gmail.com>
2026-09-22 00:36:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,10 +19,8 @@ int main()
 
   deptList[0].addCourse(Course("ITAS101", "Networking Fundamentals", "MWF 8-10", 500));
   deptList[0].addCourse(Course("ITAS201", "IT Security Basics", "TTh 2-4", 600));
-
   deptList[1].addCourse(Course("SDDS101", "Intro to Programming", "MWF 11-1", 520));
   deptList[1].addCourse(Course("SDDS201", "Object Oriented Programming", "TTh 11-1", 620));
-
   deptList[2].addCourse(Course("ECET101", "Digital Systems", "MWF 2-4", 550));
   deptList[2].addCourse(Course("ECET201", "Software Engineering", "TTh 8-10", 650));
 
